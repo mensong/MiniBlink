0f6656db146230614b150a5437cf1d389c9947db
Gergul <mail0668@gmail.com>
2018-12-21 10:57:24 +0800

    修复一处崩溃

--- wke/wkeString.cpp
+++ wke/wkeString.cpp
@@ -97,8 +97,8 @@ const wchar_t* CString::stringW() const
         const wchar_t* wtfWide = stringBuf.data();
         size_t wtfWideLen = stringBuf.size();
 
-        m_wide = new wchar_t[wtfWideLen];
-        memcpy(m_wide, wtfWide, (wtfWideLen + 1)* sizeof(wchar_t));
+        m_wide = new wchar_t[wtfWideLen + 1];
+        memcpy(m_wide, wtfWide, wtfWideLen* sizeof(wchar_t));
 
         m_wide[wtfWideLen] = 0;
     }
